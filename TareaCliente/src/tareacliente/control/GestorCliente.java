e51a41222dad37523b40079428fd9ff4552e33e0
Luis Vargas Benambourg <lusiulis@gmail.com>
2020-01-24 21:36:46 -0600

    fgh

--- TareaCliente/src/tareacliente/control/GestorCliente.java
+++ TareaCliente/src/tareacliente/control/GestorCliente.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tareacliente.control;
 
 import java.util.Observer;
 import tareacliente.model.ModeloCliente;
 
-/**
- *
- * @author Kevin
- */
 public class GestorCliente {
 
     ModeloCliente modelo;
